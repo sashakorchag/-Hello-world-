e919342e8815ee38fbbe445beba44fea29036b1e
Александр Павлович Корчагин <kor4agin.shura2017@yandex.ru>
2024-12-15 20:36:59 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sashakorchag/hello-golang
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(hello())
+}
+
+func hello() string {
+	return "Hello Go" // Changed to match the test's expectation
+}
